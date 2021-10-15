bf768bc0ee508dad3cbd1070ab44653c928af523
Mark <markgzpccw@gmail.com>
2021-10-15 21:49:12 +0800

    first-react-app

---
+++ solutions/day-03/index.js
@@ -0,0 +1,9 @@
+import doSomeMath from './math.js'
+
+import { addTwo, multiply, subtract } from './main'
+
+import * as everything from './main'
+console.log(addTwo(2, 5))
+console.log(multiply(3, 5))
+console.log(doSomeMath.subtract(5, 3))
+console.log(everything)
