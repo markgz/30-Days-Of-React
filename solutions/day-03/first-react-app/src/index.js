7c8b1d08761d654c9650622e8b8d862f4c15904a
Mark <markgzpccw@gmail.com>
2021-10-15 22:40:15 +0800

    -headerStyle,mainStyle,footerStyle

--- solutions/day-03/first-react-app/src/index.js
+++ solutions/day-03/first-react-app/src/index.js
@@ -2,6 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const rootElement = document.getElementById('root')
-const jsxElement = <h1>Welcome to 30 Days of React</h1>
+const headerStyle = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
 
-ReactDOM.render(jsxElement, rootElement)
+const header = (
+  <header style={headerStyle}>
+    <h1>Welcome to 30 Days of React</h1>
+    <h2>This is day 3 exercises</h2>
+    <h3>markgz</h3>
+    <h4>yan</h4>
+  </header>
+)
+
+const mainStyle = {
+  backgroundColor: '#F3F0F5'
+}
+const main = (
+  <main style={mainStyle}>
+    <p>Prerequisite to get started react.js</p>
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>javascript</li>
+      <li>react</li>
+    </ul>
+  </main>
+)
+
+const footerStyle = {
+  backgroundColor: '#61DBFB'
+}
+const footer = (
+  <footer style={footerStyle}>
+    <p>
+      copyright 2021
+    </p>
+  </footer>
+)
+
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+ReactDOM.render(app, rootElement)
