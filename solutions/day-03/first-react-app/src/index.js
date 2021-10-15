f2b50c72e18fdad0b58395f0aef1f03d96a6799e
Mark <markgzpccw@gmail.com>
2021-10-15 22:49:58 +0800

    inject dynamic data

--- solutions/day-03/first-react-app/src/index.js
+++ solutions/day-03/first-react-app/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const welcome = 'Welcome to 30 Days of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'markgz',
+  lastName: 'yan'
+}
+const date = 'Oct 15, 2021'
+
 const rootElement = document.getElementById('root')
 const headerStyle = {
   backgroundColor: '#61DBFB',
@@ -11,24 +21,26 @@ const headerStyle = {
 
 const header = (
   <header className='header-wrapper'>
-    <h1>Welcome to 30 Days of React</h1>
-    <h2>This is day 3 exercises</h2>
-    <h3>markgz</h3>
-    <h4>yan</h4>
+    <h1>{welcome}</h1>
+    <h2>{title}</h2>
+    <p>{subtitle}</p>
+    <h3>{author.firstName}</h3>
+    <h4>{author.lastName}</h4>
+    <small>{date}</small>
   </header>
 )
 
 const mainStyle = {
   backgroundColor: '#F3F0F5'
 }
+const techs = ['css', 'html', 'javascript', 'python', 'swift']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
 const main = (
   <main className='main-wrapper'>
     <p>Prerequisite to get started react.js</p>
     <ul>
-      <li>css</li>
-      <li>html</li>
-      <li>javascript</li>
-      <li>react</li>
+      {techsFormatted}
     </ul>
   </main>
 )
