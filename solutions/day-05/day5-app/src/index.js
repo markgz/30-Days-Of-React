7e77200764e252b071ddaebfdf8fc12de6670625
Mark <markgzpccw@gmail.com>
2021-10-17 20:09:02 +0800

    pass number type data

--- solutions/day-05/day5-app/src/index.js
+++ solutions/day-05/day5-app/src/index.js
@@ -4,26 +4,33 @@ import ReactDOM from 'react-dom';
 
 const Header = (props) => {
   console.log('props', props)
-  return (<header>
-    <h1>{props.firstName}</h1>
-    <h2>{props.lastName}</h2>
-    <h3>{props.country}</h3>
-    <p>{props.title}</p>
-    <small>{props.skills}</small>
-  </header>
-)
+  return (
+    <header>
+      <h1>{props.firstName}</h1>
+      <h2>{props.lastName}</h2>
+      <h3>{props.country}</h3>
+      <p>{props.title}</p>
+      <small>{props.skills}</small>
+    </header>
+  )
 }
 
+const Age = (props) => <div>The person is {props.age} years old</div>
+const Weight = (props) => (
+  <p>The weight of the object on earth is {props.weight} N.</p>
+)
+
 const App = () => {
-  const firstName = "markgz"
-  const lastName = "yan"
-  const country = "china"
-  const title = "mobile manager"
-  const skills = ["java", "javascript", "html", "css"]
+  let currentYear = 2021
+  let birthYear = 1990
+  const age = currentYear - birthYear
+  const gravity = 9.81
+  const mass = 75
 
   return (
-    <div>
-      <Header firstName={firstName} lastName={lastName} country={country} title={title} skills={skills.join(" + ")} />
+    <div className='app'>
+      <Age age={age} />
+      <Weight weight={gravity * mass} />
     </div>
   )
 }
