f59b845fcca6fa284b898aba6d12ab70703f816d
Mark <markgzpccw@gmail.com>
2021-10-18 21:26:34 +0800

    mapping arrary of arrays

--- solutions/day-06/day6-app/src/index.js
+++ solutions/day-06/day6-app/src/index.js
@@ -7,15 +7,32 @@ const Numbers = ({ numbers }) => {
   return list
 }
 
+const skills = [
+  ['HTML', 10],
+  ['CSS', 7],
+  ['Java', 2],
+  ['JavaScript', 10]
+]
+
+const Skill = ({ skill: [tech, level] }) => (
+  <li>
+    tech:{tech} level:{level}
+  </li>
+)
+
+const Skills = ({ skills }) => {
+  const skillsList = skills.map((skill) => <Skill skill={skill} />)
+  console.log('skillsList', skillsList)
+  return <ul>{skillsList}</ul>
+}
+
 const App = () => {
   const numbers = [1, 2, 3, 4, 5]
   return (
     <div className='container'>
       <div>
-        <h1>Numbers List</h1>
-        <ul>
-          <Numbers numbers={numbers} />
-        </ul>
+        <h1>Skills Level</h1>
+        <Skills skills={skills} />
       </div>
     </div>
   )
