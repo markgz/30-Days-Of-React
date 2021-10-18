b8f7df069054d1be7b76d03a3182149c959cd69e
Mark <markgzpccw@gmail.com>
2021-10-18 21:20:54 +0800

    mapping array of numbers

--- solutions/day-06/day6-app/src/index.js
+++ solutions/day-06/day6-app/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const Numbers = ({ numbers }) => {
+  const list = numbers.map((number) => <li>{number}</li>)
+  return list
+}
+
 const App = () => {
+  const numbers = [1, 2, 3, 4, 5]
   return (
     <div className='container'>
       <div>
         <h1>Numbers List</h1>
-        {[1, 2, 3, 4, 5]}
+        <ul>
+          <Numbers numbers={numbers} />
+        </ul>
       </div>
     </div>
   )
