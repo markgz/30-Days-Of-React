75906b8b1bb02fd96e9e81b700bf43237331a8c5
Mark <markgzpccw@gmail.com>
2021-10-19 21:40:12 +0800

    update to class component

--- solutions/day-07/day7-app/src/index.js
+++ solutions/day-07/day7-app/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <header>
@@ -17,5 +21,67 @@ class Header extends React.Component {
   }
 }
 
+class TechList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const techs = ['HTML', 'CSS', "JavaScript"]
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <main>
+        <div className="main-wrapper">
+          <p>Prerequiste to get started react.js</p>
+          <ul>
+            <TechList />
+          </ul>
+        </div>
+      </main>
+    )
+  }
+}
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>CopyRight 2021</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+}
 const rootElement = document.getElementById('root')
 ReactDOM.render(<Header />, rootElement)
